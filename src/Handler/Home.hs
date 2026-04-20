1afc5846b82f0386bcec838a5492c1e247c788ea
Young Gyu Park <younggyu.park@orionic.com>
2026-04-20 19:56:11 +0900

    Redesign homepage with creative landing UI

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell, MultiParamTypeClasses, TypeFamilies, NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, MultiParamTypeClasses, TypeFamilies, NoImplicitPrelude, QuasiQuotes #-}
 module Handler.Home where
 
 import Import
@@ -7,7 +7,11 @@ import qualified Data.Text as T
 
 getHomeR :: Handler Html
 getHomeR = do
+    mViewerId <- maybeAuthId
     latestComments <- runDB $ selectList [] [Desc WordCommentCreatedAt, LimitTo 10]
+    totalWords <- runDB $ count ([] :: [Filter Word])
+    totalStories <- runDB $ count ([] :: [Filter WordComment])
+    totalMembers <- runDB $ count ([] :: [Filter User])
     
     -- Get words for these comments
     let wordIds = ordNub $ map (wordCommentWord . entityVal) latestComments
@@ -21,4 +25,9 @@ getHomeR = do
 
     defaultLayout $ do
         setTitle "Dictionary SNS - Connect through words"
+        toWidgetHead [hamlet|
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet">
+        |]
         $(widgetFile "homepage")
